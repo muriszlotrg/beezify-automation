68710757574614e9897162b81e87d4e163b9346e
Emir Mujacic <mujacic-emir@hotmail.com>
2015-12-15 22:48:31 +0100

    Prvi i drugi dio gotovi

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,22 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTest  {
+
+	WebDriver driver;
+	
+	@BeforeMethod
+	public void testSetUp(){
+		driver = new FirefoxDriver();
+		driver.get("https://test1.beezify.io");
+	}
+	
+	@AfterMethod
+	public void testTearDown(){
+	//	driver.close();
+	}
+}
